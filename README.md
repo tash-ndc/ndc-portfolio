8ac6088120930ca86e5b37bca3a68ec629e22485
NDC <74566670+tash-ndc@users.noreply.github.com>
2020-11-30 12:06:28 +0000

    final commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Portfolio Website
+
+### Project for Codecademy
+
+A basic website showing a few of the projects I've done through the course so far.
+
+*Created in VS Code with HTML, CSS and JavaScript*
---
+++ resources/script/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $("#techvault-heading").click(function(){
+        $(".project-techvault").toggle(700);
+    });
+});
+
+$(document).ready(function(){
+    $("#mixmsg-heading").click(function(){
+        $(".project-mixmsg").toggle(700);
+    });
+});
+
+$(document).ready(function(){
+    $("#webstyles-heading").click(function(){
+        $(".project-webstyles").toggle(700);
+    });
+});
+
+$(document).ready(function(){
+    $("#mystery-heading").click(function(){
+        $(".project-mystery").toggle(700);
+    });
+});
+
+$(document).ready(function(){
+    $("#clubbird-heading").click(function(){
+        $(".project-clubbird").toggle(700);
+    });
+});
